f0089b8e48b4e61c9a46bde353d5424659a805da
oyashchyshyn <f.main.account@pm.me>
2023-09-21 21:00:49 +0300

    new updates

--- typescript/js/app.js
+++ typescript/js/app.js
@@ -1,10 +1,59 @@
 "use strict";
-function add(n1, n2) {
+function addTwoNumbers(n1, n2) {
     return n1 + n2;
 }
 const button = document.getElementById("button");
-button === null || button === void 0 ? void 0 : button.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    console.log(add(1, 1));
-});
+if (button) {
+    button.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        console.log(addTwoNumbers(1, 1));
+    });
+}
+const addMultipleNumbers = (...args) => {
+    return args.reduce((result, next) => result + next);
+};
+addMultipleNumbers(1, 2, 3, 4);
+class Department {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    describe() {
+        console.log('Department: ' + this._name);
+    }
+}
+var accounting = new Department('Accounting');
+accounting.describe();
+accounting.name = 'test';
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe();
+const accountingCopy1 = { name: 's', describe: accounting.describe };
+accountingCopy1.describe();
+const accountingCopy2 = { describe: accounting.describe.bind(accounting) };
+accountingCopy2.describe();
+let test1 = 'test';
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data = this.data.slice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+var nameDataStorage = new DataStorage();
+nameDataStorage.addItem('Max');
+nameDataStorage.addItem('Menu');
+nameDataStorage.removeItem('Max');
+console.log(nameDataStorage.getItems());
 //# sourceMappingURL=app.js.map
--- typescript/ts/app.ts
+++ typescript/ts/app.ts
@@ -16,3 +16,64 @@ const addMultipleNumbers = (...args: number[]) : number => {
 } 
 
 addMultipleNumbers(1,2,3,4)
+
+class Department {
+  private _name: string
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public describe(): void {
+    console.log('Department: ' + this._name);
+  }
+}
+
+var accounting = new Department('Accounting');
+accounting.describe();
+accounting.name = 'test';
+
+const accountingCopy = { describe: accounting.describe }
+accountingCopy.describe();
+
+const accountingCopy1 = { name: 's', describe: accounting.describe }
+accountingCopy1.describe();
+
+const accountingCopy2 = { describe: accounting.describe.bind(accounting) }
+accountingCopy2.describe();
+
+type A = number | string;
+type B = string | number;
+type C = A & B;
+
+let test1: C = 'test';
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  public addItem(item: T) {
+    this.data.push(item);
+  }
+
+  public removeItem(item: T) {
+    this.data = this.data.slice(this.data.indexOf(item), 1);
+  }
+
+  public getItems(): T[] {
+    return [...this.data];
+  }
+}
+
+var nameDataStorage = new DataStorage<string>();
+nameDataStorage.addItem('Max')
+nameDataStorage.addItem('Menu')
+nameDataStorage.removeItem('Max');
+console.log(nameDataStorage.getItems());
